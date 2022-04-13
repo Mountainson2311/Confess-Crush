8b7d521d4d7cefef6ee3700036128f997c73af8f
Mountainson2311 <103551737+Mountainson2311@users.noreply.github.com>
2022-04-13 10:43:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    titleWeb: "Trâncute",
+    introTitle: 'Hello bé',
+    introDesc: `Anh có câu hỏi này bé trả lời giúp anh nhé`,
+    btnIntro: '^^Oke anh^^',
+    title: 'Thứ 7 hay chủ nhật tuần này không biết bé có rảnh hong ta 😉',
+    desc: 'Nếu rảnh thì mình đi xem phim nhé😙 ',
+    btnYes: 'Dạ em rảnh >.<',
+    btnNo: 'Em bận mất rồi :<',
+    question: 'Tốt quá rồi vậy em nhắn cho anh thời gian em rảnh nhé <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Em rảnh lúc nào cũng được cả <33333333',
+    mess: 'Thật chất anh không đọc đươc đâu nhưng mà...',
+    messDesc: 'Mình nhắn tin thêm với nhau để bàn về buổi hẹn nhé',
+    btnAccept: 'Dạ anh <3',
+    messLink: 'https://www.facebook.com/sonngocpham2311' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
